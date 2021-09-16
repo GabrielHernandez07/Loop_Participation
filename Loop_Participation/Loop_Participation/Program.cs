d538e955d41a5802de4d212637bdda3f1560c95a
ghern <ghern@DESKTOP-OCUVVH0>
2021-09-16 11:32:44 -0500

    Guess Loop done Correctly.

---
+++ Loop_Participation/Loop_Participation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Loop_Participation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minimum, maximum, guessNumber;
+            
+            Console.WriteLine("Random number simulator");
+            Console.WriteLine("Please pick a minimum value for the range. >>");
+            string min = Console.ReadLine();
+            minimum = Convert.ToInt32(min);
+
+            Console.WriteLine("Please pick a maximum value for the rangs. >>");
+            string max = Console.ReadLine();
+            maximum = Convert.ToInt32(max);
+
+            Random rand = new Random();
+            double randValue = rand.Next(minimum, maximum + 1);
+
+            Console.WriteLine("Guess the random value within your range. >>");
+            string guess = Console.ReadLine();
+            guessNumber = Convert.ToInt32(guess);
+
+            while (guessNumber != randValue)
+            {
+                Console.WriteLine("Guessed Incorrectly. Guess agian.");
+                string newguess = Console.ReadLine();
+                guessNumber = Convert.ToInt32(newguess);
+            }
+            Console.WriteLine("Congrats you guessed correctly");
+        }
+    }
+}
